c1a652d9e24c05e756604670ffc11469afa4ae57
Omar Mufti <omufti@uxbridgecollege.ac.uk>
2022-08-15 10:29:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#stores a black arrow pointing left in variable left
+left = "\N{LEFTWARDS BLACK ARROW}"
+#stores a black arrow pointing right in variable right
+right = "\N{RIGHTWARDS BLACK ARROW}"
+#stores a black arrow pointing up in variable up
+up = "\N{UPWARDS BLACK ARROW}"
+#stores a black arrow pointing down in variable down
+down = "\N{DOWNWARDS BLACK ARROW}"
+print(left,"",right,"",up,"", down)
+#print the celsius format
+celsius = "\N{DEGREE CELSIUS}"
+print(celsius)
+#print the fahrenheit format
+fahrenheit = "\N{DEGREE FAHRENHEIT}"
+print(fahrenheit)
